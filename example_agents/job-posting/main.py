912ba9308a47f333627233603cebe3317a136b11
joao <vitor191291@gmail.com>
2024-09-15 20:05:44 -0300

    added pydantic input & output to the API

--- example_agents/job-posting/main.py
+++ example_agents/job-posting/main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from textwrap import dedent
-
+from pydantic import BaseModel
 from crewai import Crew, Agent, Task
 from crewai_tools import WebsiteSearchTool, SerperDevTool
 
@@ -11,7 +11,14 @@ load_dotenv()
 
 web_search_tool = WebsiteSearchTool()
 seper_dev_tool = SerperDevTool()
-						
+
+class InputSchema(BaseModel):
+    company_domain: str
+    company_description: str
+
+class OutputSchema(BaseModel):
+    report: str
+    		
 def main():
     # Create Agents
     researcher_agent = Agent(
@@ -24,12 +31,13 @@ def main():
     
     # Define Tasks for each agent
     research_company_culture_task = Task(
-						description=dedent(f"""\
+						description=dedent("""\
 								Analyze the provided company website and the hiring manager's company's domain {company_domain}, description: "{company_description}". Focus on understanding the company's culture, values, and mission. Identify unique selling points and specific projects or achievements highlighted on the site.
 								Compile a report summarizing these insights, specifically how they can be leveraged in a job posting to attract the right candidates."""),
 						expected_output=dedent("""\
 								A comprehensive report detailing the company's culture, values, and mission, along with specific selling points relevant to the job role. Suggestions on incorporating these insights into the job posting should be included."""),
-						agent=researcher_agent)
+						output_json=OutputSchema,
+                        agent=researcher_agent)
 
     # Instantiate the crew with a sequential process
     crew = Crew(
@@ -44,4 +52,8 @@ def main():
 if __name__ == "__main__":
     host, port = os.getenv("API_HOST"), os.getenv("API_PORT")
     
-    GenPodCrewAI(main()).run(host, int(port))
+    GenPodCrewAI(
+        crew = main(), 
+        input_schema = InputSchema(company_domain="http://google.com", company_description="big tech company"),
+        output_schema = OutputSchema
+        ).run(host, int(port))
--- gensphere-python-sdk/gensphere_python_sdk/genpod_crewai.py
+++ gensphere-python-sdk/gensphere_python_sdk/genpod_crewai.py
@@ -1,20 +1,21 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
 from pydantic import BaseModel
+from fastapi import Body, FastAPI, HTTPException
 from crewai import Crew
 
 from gensphere_python_sdk.logging_config import setup_logger
 
 logger = setup_logger(__name__)
 
-class KickoffInput(BaseModel):
-    data: dict
-
 class GenPodCrewAI:
-    def __init__(self, crew: Crew):
+    def __init__(self, crew: Crew, input_schema: BaseModel, output_schema: BaseModel):
+        self.crew = crew        
+        self.input_schema = input_schema
+        self.output_schema = output_schema
         self.app = FastAPI()
         self.setup_routes()
-        self.crew = crew
+        
         
         logger.info("GenPodCrewAI initialized with crew: %s", self.crew.name)
     
@@ -23,14 +24,14 @@ class GenPodCrewAI:
 
     def setup_routes(self):
         
-        @self.app.post("/kickoff")
-        async def kickoff_crew(input_data: KickoffInput):
-            logger.info("Received request to kickoff with inputs: %s", input_data)
+        @self.app.post("/kickoff/")
+        async def kickoff(input_data: Annotated[self.input_schema, Body()]) -> self.output_schema:
+            logger.info("Received request to kickoff with inputs: %s", input_data.model_dump())
             try:
 
-                result = self.crew.kickoff(inputs = input_data.data)
+                result = self.crew.kickoff(inputs = input_data.model_dump())
                 logger.info("Kickoff completed with result: %s", result)
-                return {"result": result}
+                return result.json_dict
             except Exception as e: 
                 logger.error("Error in kickoff endpoint: %s", str(e))
                 raise HTTPException(status_code=500, detail="Kickoff failed")
